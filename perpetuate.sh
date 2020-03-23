d16f4e90db9342520fb25162f5c11bbf2a14d1ec
Michael P. McDonnell <mmcdonnell@vmware.com>
2020-03-23 16:26:19 -0500

    Whoops - forgot the .ssh folder

--- perpetuate.sh
+++ perpetuate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 FINAL_OCTETSET="$1.$2"
-ssh-copy-id -i ~/id_rsa.pub -oStrictHostKeyChecking=no root@192.168.${FINAL_OCTETSET}
+ssh-copy-id -i ~/.ssh/id_rsa.pub -oStrictHostKeyChecking=no root@192.168.${FINAL_OCTETSET}
 scp /etc/fahclient/config.xml root@192.168.${FINAL_OCTETSET}:/etc/fahclient/config.xml
 ssh root@192.168.${FINAL_OCTETSET} '/etc/init.d/FAHClient stop; sleep 2; /etc/init.d/FAHClient start;'
