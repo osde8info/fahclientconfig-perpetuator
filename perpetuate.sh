a69f8b410f6b771a5f0490ee7eea3770a8e1de76
Michael P. McDonnell <mmcdonnell@vmware.com>
2020-03-26 11:54:34 -0500

    A lot more checking to see if work has been done. Echos added. Need to write config file as well.

--- perpetuate.sh
+++ perpetuate.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
 
-FINAL_OCTETSET="$1.$2"
-ssh-copy-id -i ~/.ssh/id_rsa.pub -oStrictHostKeyChecking=no root@192.168.${FINAL_OCTETSET}
-scp /etc/fahclient/config.xml root@192.168.${FINAL_OCTETSET}:/etc/fahclient/config.xml
-ssh root@192.168.${FINAL_OCTETSET} '/etc/init.d/FAHClient stop; sleep 2; /etc/init.d/FAHClient start;'
+# https://tinyurl.com/tmkvlx3
+
+if [ -z "${HOME}" ]; then
+  echo "ERROR: Relies on environment variables. Can't run in CRON"
+  exit 1;
+fi;
+if [ -z "${1}" ]; then
+  echo "ERROR: Need to know both octets!"
+  echo "    Example: ${0} 19 50"
+  exit 1;
+fi;
+if [ -z "${2}" ]; then
+  echo "ERROR: Need to know both octets!"
+  echo "    Example: ${0} 19 50"
+  exit 1;
+fi;
+IPBASE="192.168"
+IP="${IPBASE}.$1.$2"
+FAHSRVUSER="root"
+RSA="id_rsa.fahclient${2}"
+RSAPUB="${HOME}/.ssh/${RSA}.pub"
+FAHCONFIG="/etc/fahclient/config.xml"
+FAHINIT="/etc/init.d/FAHClient"
+if [ ! -f ${RSAPUB} ]; then
+    echo "Generating ${RSAPUB}"
+    ssh-keygen -t RSA -C "folding@michaelpmcd.com" -f ${RSA} -N ''
+fi;
+if [ ! -f ${RSAPUB} ]; then
+  echo "Copying ID into place. You'll need to type in the password for this one."
+  ssh-copy-id -i ${RSAPUB} -oStrictHostKeyChecking=no ${FAHSRVUSER}@${IP}
+fi;
+if [ -f ${FAHCONFIG} ]; then
+  scp ${FAHCONFIG} ${FAHSRVUSER}@${IP}:${FAHCONFIG}
+  ssh ${FAHSRVUSER}@${IP} "${FAHINIT} stop; sleep 2; ${FAHINIT} start;"
+fi;
